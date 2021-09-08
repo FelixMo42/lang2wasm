69819f28678c181069b540cc337e99a5072c4767
Felix Moses <felix@tacocat.com>
2021-09-07 17:22:40 -0700

    get some basic parsing to work

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,52 @@
 pub mod node;
 
 use crate::node::*;
+use std::str::Split;
+use std::iter::Peekable;
+
+fn parse(src: &str) -> Value {
+    return eat_value(&mut src.split(" ").peekable())
+}
+
+fn eat_name(toks: &mut Peekable<Split<&str>>) -> String {
+    return toks.next().unwrap().to_string()
+}
+
+fn eat_value(toks: &mut Peekable<Split<&str>>) -> Value {
+    if toks.next_if(|tok| tok == &"(").is_some() {
+        let name = eat_name(toks);
+
+        let mut args = vec! [];
+
+        while toks.next_if(|tok| tok == &")").is_none() {
+            args.push(eat_value(toks));
+        }
+
+        return match name.as_str() {
+            "*" => Value::Mul( Box::new(args[0].clone()), Box::new(args[1].clone()) ),
+            "+" => Value::Add( Box::new(args[0].clone()), Box::new(args[1].clone()) ),
+            "-" => Value::Sub( Box::new(args[0].clone()), Box::new(args[1].clone()) ),
+
+            _ => Value::Call(name, args)
+        }
+    }
+
+    return Value::Get(eat_name(toks));
+}
 
 pub fn build() -> Result<Vec<u8>, wat::Error> {
-    let main = function("main", vec![], Kind {}, Value::Number(42));
+    let funcs = vec![
+        function("mul", vec![ ("a", Kind{}), ("b", Kind {}) ], Kind {}, parse("( * a a )")),
 
-    let wat_source = format!("(module {} (export \"{}\" (func ${})))", main.to_string(), main.name, main.name);
+        function("add", vec![ ("a", Kind{}), ("b", Kind {}) ], Kind {}, parse("( + ( mul a a ) b )")),
+
+        function("main", vec![], Kind {}, Value::Call("add".to_string(), vec![Value::Number(40), Value::Number(1)])),
+    ];
+
+    let wat_source = format!(
+        "(module {} (export \"main\" (func $main)))",
+        funcs.iter().map(|func| func.to_string()).collect::<Vec<String>>().join("")
+    );
 
     return wat::parse_str(wat_source);
 }
--- src/node/value.rs
+++ src/node/value.rs
@@ -1,15 +1,35 @@
+#[derive(Clone)]
 pub enum Value {
-    Number(i32),
-    Add(Box<Value>, Box<Value>),
     Get(String),
+    Call(String, Vec<Value>),
+
+    // Consts
+    Number(i32),
+
+    // Basic wasm instructions
+    Add(Box<Value>, Box<Value>),
+    Sub(Box<Value>, Box<Value>),
+    Mul(Box<Value>, Box<Value>),
 }
 
 impl Value {
     pub fn to_string(&self) -> String {
         match self {
-            Value::Number(value) => format!("(i32.const {})", value),
-            Value::Add(a, b) => format!("(i32.add {} {})", a.to_string(), b.to_string()),
             Value::Get(name) => format!("local.get ${}", name),
+            Value::Call(name, args) => format!("(call ${} {})", name,
+                args.iter()
+                    .map(|arg| arg.to_string())
+                    .collect::<Vec<String>>()
+                    .join(" ")
+            ),
+
+            // Const
+            Value::Number(value) => format!("(i32.const {})", value),
+
+            // Basic wasm instructions
+            Value::Add(a, b) => format!("(i32.add {} {})", a.to_string(), b.to_string()),
+            Value::Sub(a, b) => format!("(i32.sub {} {})", a.to_string(), b.to_string()),
+            Value::Mul(a, b) => format!("(i32.mul {} {})", a.to_string(), b.to_string()),
         }
     }
 }
